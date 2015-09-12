365356ba819dc27fd55ba1cbbfa4e51fc29866df
Takeru Komiya <tkomiya@hotmail.com>
2015-09-12 19:27:09 -0400

    Adds a new setup script that installs all required software to run this app

---
+++ environment-setup.sh
@@ -0,0 +1,44 @@
+outputComment "Checking for and installing/updating Homebrew"
+which -s brew
+if [[ $? != 0 ]] ; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  brew update
+fi
+
+outputComment "Checking for and installing Node.js"
+which -s node
+if [[ $? != 0 ]] ; then
+  brew install node
+  mkdir ~/npm-global
+  npm config set prefix '~/npm-global'
+  echo 'export PATH=~/npm-global/bin:$PATH' >> ~/.bash_profile
+  source ~/.bash_profile
+else
+  npm update -g npm
+fi
+
+outputComment "Checking for and installing MongoDB"
+which -s mongod
+if [[ $? != 0 ]] ; then
+  brew install mongodb
+  sudo mkdir -p /data/db
+  sudo chmod -R a+rw /data
+fi
+
+outputComment "Installing required npm global packages"
+npm install -g grunt-cli
+npm install -g bower
+
+outputComment "Installing local npm packages"
+npm install
+
+function outputComment {
+	outputPoundLine
+	echo $comment
+	outputPoundLine
+}
+
+function outputPoundLine {
+	echo "####################################"
+}
