000eb70bd8d50a34b01bac8b08f7d24806bb0844
Takeru Komiya <tkomiya@hotmail.com>
2015-11-11 07:33:35 -0500

    WIP implements array saving of vocabs

--- public/modules/vocablists/services/vocablists.client.service.js
+++ public/modules/vocablists/services/vocablists.client.service.js
@@ -2,12 +2,17 @@
 
 //Vocablists service used to communicate Vocablists REST endpoints
 angular.module('vocablists').factory('Vocablists', ['$resource',
-	function($resource) {
-		return $resource('vocablists/:vocablistId', { vocablistId: '@_id'
-		}, {
-			update: {
-				method: 'PUT'
-			}
-		});
-	}
+  function($resource) {
+    return $resource(
+      'vocablists/:vocablistId',
+      {
+        vocablistId: '@_id'
+      },
+      {
+        update: {
+          method: 'PUT'
+        }
+      }
+    );
+  }
 ]);
--- public/modules/vocabs/services/vocabs.client.service.js
+++ public/modules/vocabs/services/vocabs.client.service.js
@@ -2,12 +2,17 @@
 
 //Vocabs service used to communicate Vocabs REST endpoints
 angular.module('vocabs').factory('Vocabs', ['$resource',
-	function($resource) {
-		return $resource('vocabs/:vocabId', { vocabId: '@_id'
-		}, {
-			update: {
-				method: 'PUT'
-			}
-		});
+  function($resource) {
+    return $resource(
+      'vocabs/:vocabId',
+      {
+        vocabId: '@_id'
+      },
+      {
+        update: {
+          method: 'PUT'
+        }
+      }
+    );
 	}
 ]);
