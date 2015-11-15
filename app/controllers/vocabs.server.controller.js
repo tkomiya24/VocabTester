5f5a2f00c51517a5d0d15fe6fd671089b6fb917d
Takeru Komiya <tkomiya@hotmail.com>
2015-11-15 11:13:14 -0500

    Fixes tabbing and update()

--- app/controllers/vocabs.server.controller.js
+++ app/controllers/vocabs.server.controller.js
@@ -4,110 +4,102 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	errorHandler = require('./errors.server.controller'),
-	Vocab = mongoose.model('Vocab'),
-	_ = require('lodash');
+  errorHandler = require('./errors.server.controller'),
+  Vocab = mongoose.model('Vocab'),
+  _ = require('lodash');
 
 /**
  * Create a Vocab
  */
 exports.create = function(req, res) {
-	var vocab = new Vocab(req.body);
-	vocab.user = req.user;
+  var vocab = new Vocab(req.body);
+  vocab.user = req.user;
 
-	vocab.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(vocab);
-		}
-	});
+  vocab.save(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(vocab);
+    }
+  });
 };
 
 /**
  * Show the current Vocab
  */
 exports.read = function(req, res) {
-	res.jsonp(req.vocab);
+  res.jsonp(req.vocab);
 };
 
 /**
  * Update a Vocab
  */
 exports.update = function(req, res) {
-	if (req.vocabs) {
-		for (var i = 0; i < req.vocabs.length; i++) {
-			vocabs[i].save();
-		}
-	}
-
-	var vocab = req.vocab ;
-
-	vocab = _.extend(vocab , req.body);
-
-	vocab.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(vocab);
-		}
-	});
+  var vocab = req.vocab ;
+  vocab = _.extend(vocab , req.body);
+  vocab.save(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(vocab);
+    }
+  });
 };
 
 /**
  * Delete an Vocab
  */
 exports.delete = function(req, res) {
-	var vocab = req.vocab ;
+  var vocab = req.vocab ;
 
-	vocab.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(vocab);
-		}
-	});
+  vocab.remove(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(vocab);
+    }
+  });
 };
 
 /**
  * List of Vocabs
  */
 exports.list = function(req, res) {
-	Vocab.find().sort('-created').populate('user', 'displayName').exec(function(err, vocabs) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(vocabs);
-		}
-	});
+  Vocab.find().sort('-created').populate('user', 'displayName').exec(function(err, vocabs) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(vocabs);
+    }
+  });
 };
 
 /**
  * Vocab middleware
  */
 exports.vocabByID = function(req, res, next, id) {
-	Vocab.findById(id).populate('user', 'displayName').exec(function(err, vocab) {
-		if (err) return next(err);
-		if (! vocab) return next(new Error('Failed to load Vocab ' + id));
-		req.vocab = vocab ;
-		next();
-	});
+  Vocab.findById(id).populate('user', 'displayName').exec(function(err, vocab) {
+    if (err) return next(err);
+    if (! vocab) return next(new Error('Failed to load Vocab ' + id));
+    req.vocab = vocab ;
+    next();
+  });
 };
 
 /**
  * Vocab authorization middleware
  */
 exports.hasAuthorization = function(req, res, next) {
-	if (req.vocab.user.id !== req.user.id) {
-		return res.status(403).send('User is not authorized');
-	}
-	next();
+  if (req.vocab.user.id !== req.user.id) {
+    return res.status(403).send('User is not authorized');
+  }
+  next();
 };
