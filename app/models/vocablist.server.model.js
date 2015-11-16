888c680da6d7fd0a6caf167da10f9fbc4d9191b2
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 17:07:12 -0500

    Extracts vocablist schema

--- app/models/vocablist.server.model.js
+++ app/models/vocablist.server.model.js
@@ -3,26 +3,8 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
-
-/**
- * Vocablist Schema
- */
-var VocablistSchema = new Schema({
-  name: {
-    type: String,
-    required: 'Please fill Vocablist name',
-    trim: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  vocabs: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Vocab'
-  }]
-});
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var VocablistSchema = require('../modelSchemas/vocablist.schema');
 
 mongoose.model('Vocablist', VocablistSchema);
