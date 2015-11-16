46f8089be8723ea1fa63e8a058ee556c1117753c
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 17:19:26 -0500

    Imports schemas

--- seeder.js
+++ seeder.js
@@ -2,8 +2,12 @@
 var mongoose = require('mongoose');
 var vocablists = require('./database-backup/seed.json');
 var rsvp = require('rsvp');
+var VocablistSchema = require('./app/modelSchemas/vocablist.schema');
+var VocabSchema = require('./app/modelSchemas/vocab.schema');
 //launch mongodb
 mongoose.connect('mongodb://localhost/vocabtester-dev');
+mongoose.model('Vocab', VocabSchema);
+mongoose.model('Vocablist', VocablistSchema);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback) {
