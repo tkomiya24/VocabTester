e01da2387360d23c55006f2f7d59107cf314ff19
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 15:59:56 -0500

    Initial commit of seeder script

---
+++ seeder.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+
+//connect to database
+//retrieve seed.json
+//add all vocab objects to database
+//for all vocablists, find the vocabs and add their reference
