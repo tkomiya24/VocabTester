d0163b13b65bf4a5bec743b9a5cc242a955b6673
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 16:26:30 -0500

    Fixes connection and adds cleaning

--- seeder.js
+++ seeder.js
@@ -1,7 +1,13 @@
+/* jshint strict: false */
 var mongoose = require('mongoose');
 var vocablists = require('./database-backup/seed.json');
+var rsvp = require('rsvp');
 //launch mongodb
 mongoose.connect('mongodb://localhost/vocabtester-dev');
-//clean database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+  db.db.dropDatabase();
+});
 //add all vocab objects to database
 //for all vocablists, find the vocabs and add their reference
