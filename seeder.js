7102460e1859fb00073e264fe66459a64aa009a5
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 16:09:47 -0500

    Reads seed.json file

--- seeder.js
+++ seeder.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+var vocablists = require('./database-backup/seed.json');
+//launch mongodb
 mongoose.connect('mongodb://localhost/vocabtester-dev');
-//retrieve seed.json
 //clean database
 //add all vocab objects to database
 //for all vocablists, find the vocabs and add their reference
