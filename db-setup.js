14eacc08869b6e4e11adff303052c5fbce5cd596
Takeru Komiya <tkomiya@hotmail.com>
2015-11-15 19:53:45 -0500

    Fixes seeding

--- db-setup.js
+++ db-setup.js
@@ -3,6 +3,17 @@
 db.vocablists.find({}).forEach(function(vocablist) {
   var idArray = [];
   if (vocablist.vocab && vocablist.vocab.length > 0) {
+    vocablist.vocab.forEach(function(vocab) {
+      if (vocab.english && vocab.english.lastTested.length === 0) {
+        delete vocab.english.lastTested;
+      }
+      if (vocab.korean && vocab.korean.lastTested.length === 0) {
+        delete vocab.korean.lastTested;
+      }
+      if (vocab.japanese && vocab.japanese.lastTested.length === 0) {
+        delete vocab.japanese.lastTested;
+      }
+    });
     db.vocabs.insert(vocablist.vocab, {ordered: false});
   }
   vocablist.vocab.forEach(function(vocab) {
