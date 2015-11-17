8b219fbd42de6a6f860621b3a187953b79607079
Takeru Komiya <tkomiya@hotmail.com>
2015-11-16 20:28:36 -0500

    Implements creating of vocabs

--- seeder.js
+++ seeder.js
@@ -4,14 +4,57 @@ var vocablists = require('./database-backup/seed.json');
 var rsvp = require('rsvp');
 var VocablistSchema = require('./app/modelSchemas/vocablist.schema');
 var VocabSchema = require('./app/modelSchemas/vocab.schema');
-//launch mongodb
-mongoose.connect('mongodb://localhost/vocabtester-dev');
-mongoose.model('Vocab', VocabSchema);
-mongoose.model('Vocablist', VocablistSchema);
+var Vocab = mongoose.model('Vocab', VocabSchema);
+var Vocablist = mongoose.model('Vocablist', VocablistSchema);
 var db = mongoose.connection;
+
+function getAllVocabs() {
+  var vocabs = [];
+  for (var i = 0; i < vocablists.length; i++) {
+    vocabs.push.apply(vocabs, vocablists[i].vocab);
+  }
+  return vocabs;
+}
+
+function saveVocabs() {
+  var vocabs = getAllVocabs();
+  return new rsvp.Promise(function(res, err) {
+    Vocab.create(vocabs, function(error, result) {
+      if (error) {
+        err(error);
+      } else if (!result) {
+        err('Result was empty...');
+      } else {
+        res(result);
+      }
+    });
+  });
+}
+
+function dropDatabase() {
+  return new rsvp.Promise(function(res, err) {
+    db.db.dropDatabase(function(error) {
+      if (error) {
+        err(error);
+      } else {
+        res();
+      }
+    });
+  });
+}
+
+function exit() {
+  process.exit();
+}
+
+function exitError(error) {
+  console.log(error);
+  process.exit(1);
+}
+mongoose.connect('mongodb://localhost/vocabtester-dev');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback) {
-  db.db.dropDatabase();
+  dropDatabase().then(saveVocabs).then(exit).catch(exitError);
 });
 //add all vocab objects to database
 //for all vocablists, find the vocabs and add their reference
