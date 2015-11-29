77f15878d7f1808f650e5b01ff4565d2d5f8ce79
Takeru Komiya <tkomiya@hotmail.com>
2015-11-29 15:11:32 -0500

    Changes translation to be a single value

--- app/modelSchemas/vocab.schema.js
+++ app/modelSchemas/vocab.schema.js
@@ -7,11 +7,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var supportedLanguages = ['English', 'Japanese', 'Korean'];
 var translationSchema = {
-  translation: [{
+  translation: {
     type: String,
     trim: true,
     required: 'Translation is requied'
-  }],
+  },
   timesTested: {
     type: Number
   },
