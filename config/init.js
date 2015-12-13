6e50774aeba4ef8aef2bd8989c725bb6d31a8d05
Takeru Komiya <tkomiya@hotmail.com>
2015-12-13 17:00:41 -0500

    Breaks up long lines

--- config/init.js
+++ config/init.js
@@ -19,13 +19,16 @@ module.exports = function() {
   }, function(err, environmentFiles) {
     if (!environmentFiles.length) {
       if (process.env.NODE_ENV) {
-        console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
+        console.error(chalk.red(
+          'No configuration file found for "' + process.env.NODE_ENV +
+          '" environment using development instead'));
       } else {
         console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
       }
       process.env.NODE_ENV = 'development';
     } else {
-      console.log(chalk.black.bgWhite('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
+      console.log(chalk.black.bgWhite(
+        'Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
     }
   });
 
