26da129b7e932fce7d757f4ed273d3b70e5d45a2
Takeru Komiya <takeru.komiya@sap.com>
2015-12-15 13:15:08 -0500

    Fixes tabbing

--- config/env/all.js
+++ config/env/all.js
@@ -13,33 +13,33 @@ module.exports = {
   assets: {
     lib: {
       css: [
-      'public/lib/bootstrap/dist/css/bootstrap.css',
-      'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
       ],
       js: [
-      'public/lib/angular/angular.js',
-      'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-cookies/angular-cookies.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-touch/angular-touch.js',
         'public/lib/angular-sanitize/angular-sanitize.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
-      'public/lib/angular-ui-utils/ui-utils.js',
-      'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
       ]
     },
     css: [
-    'public/modules/**/css/*.css'
+      'public/modules/**/css/*.css'
     ],
     js: [
-    'public/config.js',
-    'public/application.js',
-    'public/modules/*/*.js',
-    'public/modules/*/*[!tests]*/*.js'
+      'public/config.js',
+      'public/application.js',
+      'public/modules/*/*.js',
+      'public/modules/*/*[!tests]*/*.js'
     ],
     tests: [
-    'public/lib/angular-mocks/angular-mocks.js',
-    'public/modules/*/tests/*.js'
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/modules/*/tests/*.js'
     ]
   }
 };
