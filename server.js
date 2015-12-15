fc09e3a39288b77769f66cb8807b6f5d3d148330
Takeru Komiya <takeru.komiya@sap.com>
2015-12-15 13:19:57 -0500

    Delints

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 /**
  * Module dependencies.
  */
-var init = require('./config/init')(),
-  config = require('./config/config'),
-  mongoose = require('mongoose'),
-  chalk = require('chalk');
+var init = require('./config/init')();
+var config = require('./config/config');
+var mongoose = require('mongoose');
+var chalk = require('chalk');
 
 /**
  * Main application entry file.
