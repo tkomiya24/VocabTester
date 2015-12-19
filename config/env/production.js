63e55f64bafc6dd71bafba5fecbc6b5e919c1112
Takeru Komiya <tkomiya@hotmail.com>
2015-12-19 10:46:33 -0500

    Fixes linting

--- config/env/production.js
+++ config/env/production.js
@@ -1,21 +1,23 @@
 'use strict';
 
 module.exports = {
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/vocabtester',
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI ||
+      'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR ||
+      'localhost') + '/vocabtester',
   assets: {
     lib: {
       css: [
       'public/lib/bootstrap/dist/css/bootstrap.min.css',
-      'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+      'public/lib/bootstrap/dist/css/bootstrap-theme.min.css'
       ],
       js: [
       'public/lib/angular/angular.min.js',
       'public/lib/angular-resource/angular-resource.js',
-				'public/lib/angular-cookies/angular-cookies.js',
-				'public/lib/angular-animate/angular-animate.js',
-				'public/lib/angular-touch/angular-touch.js',
-				'public/lib/angular-sanitize/angular-sanitize.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-cookies/angular-cookies.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-touch/angular-touch.js',
+        'public/lib/angular-sanitize/angular-sanitize.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
       'public/lib/angular-ui-utils/ui-utils.min.js',
       'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
       ]
