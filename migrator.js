be4716bbff6cc3763b74c03632ef49ed553942e4
Takeru Komiya <tkomiya@hotmail.com>
2015-12-29 15:57:47 -0800

    Adds a migrator class

---
+++ migrator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports.convertVocab = function(vocab) {
+
+  var refactoredVocab = {};
+  refactoredVocab.word = vocab.english.translation;
+  var alternateLanguage = vocab.japanese ? 'japanese' : 'korean';
+
+  refactoredVocab.translation = vocab[alternateLanguage].translation;
+  refactoredVocab.timesTested = vocab[alternateLanguage].timesTested;
+  refactoredVocab.timesCorrect = vocab[alternateLanguage].timesCorrect;
+  refactoredVocab.lastTested = vocab[alternateLanguage].lastTested | '';
+  return refactoredVocab;
+};
