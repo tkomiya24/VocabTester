1780ce0547dff3da0ca59318f271d67a81cbe817
Takeru Komiya <tkomiya@hotmail.com>
2015-12-29 15:18:26 -0800

    Fixes formatting

--- public/modules/vocablists/config/vocablists.client.routes.js
+++ public/modules/vocablists/config/vocablists.client.routes.js
@@ -2,28 +2,28 @@
 
 //Setting up route
 angular.module('vocablists').config(['$stateProvider',
-	function($stateProvider) {
-  // Vocablists state routing
-  $stateProvider.
-		state('listVocablists', {
-  url: '/vocablists',
-  templateUrl: 'modules/vocablists/views/list-vocablists.client.view.html'
-		}).
-		state('createVocablist', {
-  url: '/vocablists/create',
-  templateUrl: 'modules/vocablists/views/create-vocablist.client.view.html'
-		}).
-		state('viewVocablist', {
-  url: '/vocablists/:vocablistId',
-  templateUrl: 'modules/vocablists/views/list-vocablists.client.view.html'
-		}).
-		state('editVocablist', {
-			url: '/vocablists/:vocablistId/edit',
-			templateUrl: 'modules/vocablists/views/edit-vocablist.client.view.html'
-		}).
-		state('testVocablist', {
-			url: '/vocablists/:vocablistId/test',
-			templateUrl: 'modules/vocablists/views/test-vocablist.client.view.html'
-		});
-	}
+  function($stateProvider) {
+    // Vocablists state routing
+    $stateProvider.
+      state('listVocablists', {
+        url: '/vocablists',
+        templateUrl: 'modules/vocablists/views/list-vocablists.client.view.html'
+      }).
+      state('createVocablist', {
+        url: '/vocablists/create',
+        templateUrl: 'modules/vocablists/views/create-vocablist.client.view.html'
+      }).
+      state('viewVocablist', {
+        url: '/vocablists/:vocablistId',
+        templateUrl: 'modules/vocablists/views/list-vocablists.client.view.html'
+      }).
+      state('editVocablist', {
+        url: '/vocablists/:vocablistId/edit',
+        templateUrl: 'modules/vocablists/views/edit-vocablist.client.view.html'
+      }).
+      state('testVocablist', {
+        url: '/vocablists/:vocablistId/test',
+        templateUrl: 'modules/vocablists/views/test-vocablist.client.view.html'
+      });
+  }
 ]);
