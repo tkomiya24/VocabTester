de7a4d22df86d37dc7a78a3fd1e80ab923e1a135
Takeru Komiya <tkomiya@hotmail.com>
2016-01-01 17:26:24 -0800

    Adds initalization of vocab array

--- public/modules/vocablists/controllers/createVocablist.client.controller.js
+++ public/modules/vocablists/controllers/createVocablist.client.controller.js
@@ -6,6 +6,7 @@ angular.module('vocablists').
     function($scope, $stateParams, $location, Authentication, Vocablists) {
 
       $scope.vocablist = new Vocablists();
+      $scope.vocablist.vocab = [];
 
       $scope.addVocab = function() {
         $scope.vocablist.vocab.push({word: 'Word', translation: 'Translation'});
