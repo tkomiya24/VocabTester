24b48165acbe654c6779aa315afd96d02f8f1639
Takeru Komiya <tkomiya@hotmail.com>
2016-01-05 19:25:10 -0800

    Adds simplified methods for updating vocablists

--- app/controllers/vocablists.server.controller.js
+++ app/controllers/vocablists.server.controller.js
@@ -27,6 +27,36 @@ function findVocabPromise(vocab) {
   });
 }
 
+function findAndUpdateVocab(vocab) {
+  if (vocab._id) {
+    return new Promise(function(resolve, reject) {
+      Vocab.findByIdAndUpdate(
+        vocab._id,
+        vocab,
+        function(err, doc) {
+          if (err) {
+            reject(err);
+          } else if (!doc) {
+            reject(new Error('Failed to update vocab ' + vocab.word));
+          } else {
+            resolve(doc);
+          }
+        }
+      );
+    });
+  } else {
+    return Vocab.create(vocab);
+  }
+}
+
+function findAndUpdateVocabs(vocabs) {
+  var promises = [];
+  for (var i = 0; i < vocabs.length; i++) {
+    promises.push(findAndUpdateVocab(vocabs[i]));
+  }
+  return Promise.all(promises);
+}
+
 function makeFindVocabPromises(vocabs) {
   var promises = [];
   for (var i = 0; i < vocabs.length; i++) {
