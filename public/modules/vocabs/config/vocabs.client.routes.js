14ff957cae9f5b119c735d62314f65f5d2c9989f
Takeru Komiya <tkomiya@hotmail.com>
2016-01-19 15:15:22 +0900

    removes tabs

--- public/modules/vocabs/config/vocabs.client.routes.js
+++ public/modules/vocabs/config/vocabs.client.routes.js
@@ -2,24 +2,24 @@
 
 //Setting up route
 angular.module('vocabs').config(['$stateProvider',
-	function($stateProvider) {
+  function($stateProvider) {
   // Vocabs state routing
   $stateProvider.
-		state('listVocabs', {
+    state('listVocabs', {
   url: '/vocabs',
   templateUrl: 'modules/vocabs/views/list-vocabs.client.view.html'
-		}).
-		state('createVocab', {
+    }).
+    state('createVocab', {
   url: '/vocabs/create',
   templateUrl: 'modules/vocabs/views/create-vocab.client.view.html'
-		}).
-		state('viewVocab', {
+    }).
+    state('viewVocab', {
   url: '/vocabs/:vocabId',
   templateUrl: 'modules/vocabs/views/view-vocab.client.view.html'
-		}).
-		state('editVocab', {
+    }).
+    state('editVocab', {
   url: '/vocabs/:vocabId/edit',
   templateUrl: 'modules/vocabs/views/edit-vocab.client.view.html'
-		});
-	}
+    });
+  }
 ]);
