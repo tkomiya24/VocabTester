f141d48edf741e20083ff3c8737f96dce154da6b
Takeru Komiya <tkomiya@hotmail.com>
2016-01-19 16:19:39 +0900

    Removes route for creating Vocabs

--- public/modules/vocabs/config/vocabs.client.routes.js
+++ public/modules/vocabs/config/vocabs.client.routes.js
@@ -9,10 +9,6 @@ angular.module('vocabs').config(['$stateProvider',
         url: '/vocabs',
         templateUrl: 'modules/vocabs/views/list-vocabs.client.view.html'
       }).
-      state('createVocab', {
-        url: '/vocabs/create',
-        templateUrl: 'modules/vocabs/views/create-vocab.client.view.html'
-      }).
       state('viewVocab', {
         url: '/vocabs/:vocabId',
         templateUrl: 'modules/vocabs/views/view-vocab.client.view.html'
