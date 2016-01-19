2d0e051252821b9b8c65a5961b9d91785968879c
Takeru Komiya <tkomiya@hotmail.com>
2016-01-19 15:14:19 +0900

    Fixes tabbing

--- public/modules/vocabs/config/vocabs.client.config.js
+++ public/modules/vocabs/config/vocabs.client.config.js
@@ -3,9 +3,9 @@
 // Configuring the Articles module
 angular.module('vocabs').run(['Menus',
   function(Menus) {
-  // Set top bar menu items
-  Menus.addMenuItem('topbar', 'Vocabs', 'vocabs', 'dropdown', '/vocabs(/create)?');
-  Menus.addSubMenuItem('topbar', 'vocabs', 'List Vocabs', 'vocabs');
-  Menus.addSubMenuItem('topbar', 'vocabs', 'New Vocab', 'vocabs/create');
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', 'Vocabs', 'vocabs', 'dropdown', '/vocabs(/create)?');
+    Menus.addSubMenuItem('topbar', 'vocabs', 'List Vocabs', 'vocabs');
+    Menus.addSubMenuItem('topbar', 'vocabs', 'New Vocab', 'vocabs/create');
   }
 ]);
