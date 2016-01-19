9f179102b811816e04caf858800be4776b452ab2
Takeru Komiya <tkomiya@hotmail.com>
2016-01-19 15:14:02 +0900

    Untabifies

--- public/modules/vocabs/config/vocabs.client.config.js
+++ public/modules/vocabs/config/vocabs.client.config.js
@@ -2,10 +2,10 @@
 
 // Configuring the Articles module
 angular.module('vocabs').run(['Menus',
-	function(Menus) {
+  function(Menus) {
   // Set top bar menu items
   Menus.addMenuItem('topbar', 'Vocabs', 'vocabs', 'dropdown', '/vocabs(/create)?');
   Menus.addSubMenuItem('topbar', 'vocabs', 'List Vocabs', 'vocabs');
   Menus.addSubMenuItem('topbar', 'vocabs', 'New Vocab', 'vocabs/create');
-	}
+  }
 ]);
