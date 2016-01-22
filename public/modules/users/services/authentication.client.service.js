51346ba646cb0434843f1c6eee3bf3ae49180b51
Takeru Komiya <tkomiya@hotmail.com>
2016-01-22 11:29:18 -0800

    Adds a method to redirect unauthenticated users

--- public/modules/users/services/authentication.client.service.js
+++ public/modules/users/services/authentication.client.service.js
@@ -1,12 +1,18 @@
 'use strict';
 
 // Authentication service for user variables
-angular.module('users').factory('Authentication', [
-  function() {
+angular.module('users').factory('Authentication', ['$location', '$rootScope',
+  function($location, $rootScope) {
     var _this = this;
 
     _this._data = {
-      user: window.user
+      user: window.user,
+      redirectUnauthenticated: function() {
+        if (!window.user) {
+          $rootScope.authenticationMessage = 'Sorry, you must be signed in to view that page';
+          $location.path('/signin');
+        }
+      }
     };
 
     return _this._data;
