f16dede287fae38e2c3c6ef35291e40e595d4aac
Takeru Komiya <tkomiya@hotmail.com>
2016-01-22 21:15:55 -0800

    Fixes tabbing

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -2,17 +2,17 @@
 
 angular.module('core').controller('HeaderController', ['$scope','Authentication', 'Menus',
   function($scope, Authentication, Menus) {
-  $scope.authentication = Authentication;
-  $scope.isCollapsed = false;
-  $scope.menu = Menus.getMenu('topbar');
-
-  $scope.toggleCollapsibleMenu = function() {
-    $scope.isCollapsed = !$scope.isCollapsed;
-  };
-
-  // Collapsing the menu after navigation
-  $scope.$on('$stateChangeSuccess', function() {
+    $scope.authentication = Authentication;
     $scope.isCollapsed = false;
-  });
+    $scope.menu = Menus.getMenu('topbar');
+
+    $scope.toggleCollapsibleMenu = function() {
+      $scope.isCollapsed = !$scope.isCollapsed;
+    };
+
+    // Collapsing the menu after navigation
+    $scope.$on('$stateChangeSuccess', function() {
+      $scope.isCollapsed = false;
+    });
   }
 ]);
