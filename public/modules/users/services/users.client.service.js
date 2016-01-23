0ed9aedb830eba10a9508ac4268a1fcdf313cca6
Takeru Komiya <tkomiya@hotmail.com>
2016-01-22 21:13:27 -0800

    Fixes tabbing

--- public/modules/users/services/users.client.service.js
+++ public/modules/users/services/users.client.service.js
@@ -3,10 +3,10 @@
 // Users service used for communicating with the users REST endpoint
 angular.module('users').factory('Users', ['$resource',
   function($resource) {
-  return $resource('users', {}, {
-    update: {
-      method: 'PUT'
-    }
-  });
+    return $resource('users', {}, {
+      update: {
+        method: 'PUT'
+      }
+    });
   }
 ]);
