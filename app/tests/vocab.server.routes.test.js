374529b0b5978dffa4da5ee75854e7243f8f122a
Takeru Komiya <tkomiya@hotmail.com>
2016-01-23 10:13:15 -0800

    Fixes indentation

--- app/tests/vocab.server.routes.test.js
+++ app/tests/vocab.server.routes.test.js
@@ -51,51 +51,51 @@ describe('Vocab CRUD tests', function() {
     agent.post('/auth/signin')
     .send(credentials)
     .expect(200)
-			.end(function(signinErr, signinRes) {
-  // Handle signin error
-  if (signinErr) {
-    done(signinErr);
-  }
-  // Get the userId
-  var userId = user.id;
+    .end(function(signinErr, signinRes) {
+      // Handle signin error
+      if (signinErr) {
+        done(signinErr);
+      }
+      // Get the userId
+      var userId = user.id;
 
-  // Save a new Vocab
-  agent.post('/vocabs')
-  .send(vocab)
-  .expect(200)
-					.end(function(vocabSaveErr, vocabSaveRes) {
-  // Handle Vocab save error
-  if (vocabSaveErr) {
-    done(vocabSaveErr);
-  }
-  // Get a list of Vocabs
-  agent.get('/vocabs')
-							.end(function(vocabsGetErr, vocabsGetRes) {
-  // Handle Vocab save error
-  if (vocabsGetErr) {
-    done(vocabsGetErr);
-  }
-  // Get Vocabs list
-  var vocabs = vocabsGetRes.body;
-  // Set assertions
-  (vocabs[0].user._id).should.equal(userId);
-  (vocabs[0].name).should.match('Vocab Name');
+      // Save a new Vocab
+      agent.post('/vocabs')
+      .send(vocab)
+      .expect(200)
+      .end(function(vocabSaveErr, vocabSaveRes) {
+        // Handle Vocab save error
+        if (vocabSaveErr) {
+          done(vocabSaveErr);
+        }
+        // Get a list of Vocabs
+        agent.get('/vocabs')
+        .end(function(vocabsGetErr, vocabsGetRes) {
+          // Handle Vocab save error
+          if (vocabsGetErr) {
+            done(vocabsGetErr);
+          }
+          // Get Vocabs list
+          var vocabs = vocabsGetRes.body;
+          // Set assertions
+          (vocabs[0].user._id).should.equal(userId);
+          (vocabs[0].name).should.match('Vocab Name');
 
-  // Call the assertion callback
-  done();
-							});
-					});
-			});
+          // Call the assertion callback
+          done();
+        });
+      });
+    });
   });
 
   it('should not be able to save Vocab instance if not logged in', function(done) {
     agent.post('/vocabs')
     .send(vocab)
     .expect(401)
-			.end(function(vocabSaveErr, vocabSaveRes) {
-  // Call the assertion callback
-  done(vocabSaveErr);
-			});
+    .end(function(vocabSaveErr, vocabSaveRes) {
+      // Call the assertion callback
+      done(vocabSaveErr);
+    });
   });
 
   it('should not be able to save Vocab instance if no name is provided', function(done) {
@@ -105,72 +105,72 @@ describe('Vocab CRUD tests', function() {
     agent.post('/auth/signin')
     .send(credentials)
     .expect(200)
-			.end(function(signinErr, signinRes) {
-  // Handle signin error
-  if (signinErr) {
-    done(signinErr);
-  }
+    .end(function(signinErr, signinRes) {
+      // Handle signin error
+      if (signinErr) {
+        done(signinErr);
+      }
 
-  // Get the userId
-  var userId = user.id;
+      // Get the userId
+      var userId = user.id;
 
-  // Save a new Vocab
-  agent.post('/vocabs')
-  .send(vocab)
-  .expect(400)
-					.end(function(vocabSaveErr, vocabSaveRes) {
-  // Set message assertion
-  (vocabSaveRes.body.message).should.match('Please fill Vocab name');
+      // Save a new Vocab
+      agent.post('/vocabs')
+      .send(vocab)
+      .expect(400)
+      .end(function(vocabSaveErr, vocabSaveRes) {
+        // Set message assertion
+        (vocabSaveRes.body.message).should.match('Please fill Vocab name');
 
-  // Handle Vocab save error
-  done(vocabSaveErr);
-					});
-			});
+        // Handle Vocab save error
+        done(vocabSaveErr);
+      });
+    });
   });
 
   it('should be able to update Vocab instance if signed in', function(done) {
     agent.post('/auth/signin')
     .send(credentials)
     .expect(200)
-			.end(function(signinErr, signinRes) {
-  // Handle signin error
-  if (signinErr) {
-    done(signinErr);
-  }
+    .end(function(signinErr, signinRes) {
+      // Handle signin error
+      if (signinErr) {
+        done(signinErr);
+      }
 
-  // Get the userId
-  var userId = user.id;
+      // Get the userId
+      var userId = user.id;
 
-  // Save a new Vocab
-  agent.post('/vocabs')
-  .send(vocab)
-  .expect(200)
-					.end(function(vocabSaveErr, vocabSaveRes) {
-  // Handle Vocab save error
-  if (vocabSaveErr) {
-    done(vocabSaveErr);
-  }
+      // Save a new Vocab
+      agent.post('/vocabs')
+      .send(vocab)
+      .expect(200)
+      .end(function(vocabSaveErr, vocabSaveRes) {
+        // Handle Vocab save error
+        if (vocabSaveErr) {
+          done(vocabSaveErr);
+        }
 
-  // Update Vocab name
-  vocab.name = 'WHY YOU GOTTA BE SO MEAN?';
+        // Update Vocab name
+        vocab.name = 'WHY YOU GOTTA BE SO MEAN?';
 
-  // Update existing Vocab
-  agent.put('/vocabs/' + vocabSaveRes.body._id)
-  .send(vocab)
-  .expect(200)
-							.end(function(vocabUpdateErr, vocabUpdateRes) {
-  // Handle Vocab update error
-  if (vocabUpdateErr) {
-    done(vocabUpdateErr);
-  }
-  // Set assertions
-  (vocabUpdateRes.body._id).should.equal(vocabSaveRes.body._id);
-  (vocabUpdateRes.body.name).should.match('WHY YOU GOTTA BE SO MEAN?');
-  // Call the assertion callback
-  done();
-							});
-					});
-			});
+        // Update existing Vocab
+        agent.put('/vocabs/' + vocabSaveRes.body._id)
+        .send(vocab)
+        .expect(200)
+        .end(function(vocabUpdateErr, vocabUpdateRes) {
+          // Handle Vocab update error
+          if (vocabUpdateErr) {
+            done(vocabUpdateErr);
+          }
+          // Set assertions
+          (vocabUpdateRes.body._id).should.equal(vocabSaveRes.body._id);
+          (vocabUpdateRes.body.name).should.match('WHY YOU GOTTA BE SO MEAN?');
+          // Call the assertion callback
+          done();
+        });
+      });
+    });
   });
 
   it('should be able to get a list of Vocabs if not signed in', function(done) {
@@ -181,14 +181,13 @@ describe('Vocab CRUD tests', function() {
     vocabObj.save(function() {
       // Request Vocabs
       request(app).get('/vocabs')
-				.end(function(req, res) {
-  // Set assertion
-  res.body.should.be.an.Array.with.lengthOf(1);
+        .end(function(req, res) {
+          // Set assertion
+          res.body.should.be.an.Array.with.lengthOf(1);
 
-  // Call the assertion callback
-  done();
-				});
-
+          // Call the assertion callback
+          done();
+        });
     });
   });
 
@@ -199,13 +198,13 @@ describe('Vocab CRUD tests', function() {
     // Save the Vocab
     vocabObj.save(function() {
       request(app).get('/vocabs/' + vocabObj._id)
-				.end(function(req, res) {
-  // Set assertion
-  res.body.should.be.an.Object.with.property('name', vocab.name);
+        .end(function(req, res) {
+          // Set assertion
+          res.body.should.be.an.Object.with.property('name', vocab.name);
 
-  // Call the assertion callback
-  done();
-				});
+          // Call the assertion callback
+          done();
+        });
     });
   });
 
@@ -213,41 +212,41 @@ describe('Vocab CRUD tests', function() {
     agent.post('/auth/signin')
     .send(credentials)
     .expect(200)
-			.end(function(signinErr, signinRes) {
-  // Handle signin error
-  if (signinErr) {
-    done(signinErr);
-  }
+    .end(function(signinErr, signinRes) {
+      // Handle signin error
+      if (signinErr) {
+        done(signinErr);
+      }
 
-  // Get the userId
-  var userId = user.id;
+      // Get the userId
+      var userId = user.id;
 
-  // Save a new Vocab
-  agent.post('/vocabs')
-  .send(vocab)
-  .expect(200)
-					.end(function(vocabSaveErr, vocabSaveRes) {
-  // Handle Vocab save error
-  if (vocabSaveErr) {
-    done(vocabSaveErr);
-  }
-  // Delete existing Vocab
-  agent.delete('/vocabs/' + vocabSaveRes.body._id)
-  .send(vocab)
-  .expect(200)
-							.end(function(vocabDeleteErr, vocabDeleteRes) {
-  // Handle Vocab error error
-  if (vocabDeleteErr) {
-    done(vocabDeleteErr);
-  }
-  // Set assertions
-  (vocabDeleteRes.body._id).should.equal(vocabSaveRes.body._id);
+      // Save a new Vocab
+      agent.post('/vocabs')
+      .send(vocab)
+      .expect(200)
+      .end(function(vocabSaveErr, vocabSaveRes) {
+        // Handle Vocab save error
+        if (vocabSaveErr) {
+          done(vocabSaveErr);
+        }
+        // Delete existing Vocab
+        agent.delete('/vocabs/' + vocabSaveRes.body._id)
+        .send(vocab)
+        .expect(200)
+        .end(function(vocabDeleteErr, vocabDeleteRes) {
+          // Handle Vocab error error
+          if (vocabDeleteErr) {
+            done(vocabDeleteErr);
+          }
+          // Set assertions
+          (vocabDeleteRes.body._id).should.equal(vocabSaveRes.body._id);
 
-  // Call the assertion callback
-  done();
-							});
-					});
-			});
+          // Call the assertion callback
+          done();
+        });
+      });
+    });
   });
 
   it('should not be able to delete Vocab instance if not signed in', function(done) {
@@ -262,14 +261,13 @@ describe('Vocab CRUD tests', function() {
       // Try deleting Vocab
       request(app).delete('/vocabs/' + vocabObj._id)
       .expect(401)
-			.end(function(vocabDeleteErr, vocabDeleteRes) {
-  // Set message assertion
-  (vocabDeleteRes.body.message).should.match('User is not logged in');
+      .end(function(vocabDeleteErr, vocabDeleteRes) {
+        // Set message assertion
+        (vocabDeleteRes.body.message).should.match('User is not logged in');
 
-  // Handle Vocab error error
-  done(vocabDeleteErr);
-			});
-
+        // Handle Vocab error error
+        done(vocabDeleteErr);
+      });
     });
   });
 
