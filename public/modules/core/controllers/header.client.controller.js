2cc742dcd740a55980f19e7777539631e4f4baf4
Takeru Komiya <tkomiya@hotmail.com>
2016-01-24 16:24:13 -0800

    Adds a signout method

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,13 +1,26 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope','Authentication', 'Menus',
-  function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController',
+  ['$scope','Authentication', 'Menus', '$location', '$http',
+  function($scope, Authentication, Menus, $location, $http) {
     $scope.authentication = Authentication;
     $scope.isCollapsed = false;
     $scope.menu = Menus.getMenu('topbar');
 
     $scope.toggleCollapsibleMenu = function() {
       $scope.isCollapsed = !$scope.isCollapsed;
+    };
+
+    $scope.signout = function() {
+      $http.post('/auth/signout', $scope.credentials).success(
+        function(response) {
+          delete Authentication.user;
+          $location.path('/');
+        }).error(
+        function(response) {
+          $scope.error = 'There was an error. Could not sign out. ' + response;
+        }
+      );
     };
 
     // Collapsing the menu after navigation
