c8e8bc20e83652e6fa530cd78165688798ac486b
Takeru Komiya <tkomiya@hotmail.com>
2016-01-24 19:22:53 -0800

    Fixes authorization check

--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -3,10 +3,8 @@
 angular.module('users').controller('SettingsController',
   ['$scope', '$location', 'Users', 'Authentication',
   function($scope, $location, Users, Authentication) {
-    $scope.user = Authentication.user;
-
     // If user is not signed in then redirect back home
-    if (!$scope.user) {
+    if (!Authentication.currentUser()) {
       $location.path('/');
     }
 
