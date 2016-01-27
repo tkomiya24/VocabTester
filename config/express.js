0142801f5848ea4e2ba61f83125ed2bdd1e55699
Takeru Komiya <tkomiya@hotmail.com>
2016-01-26 22:16:49 -0800

    Surrounds if with a curly brace

--- config/express.js
+++ config/express.js
@@ -121,7 +121,9 @@ module.exports = function(db) {
   // Assume 'not found' in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
   app.use(function(err, req, res, next) {
     // If the error object doesn't exists
-    if (!err) return next();
+    if (!err) {
+      return next();
+    }
 
     // Log it
     console.error(err.stack);
