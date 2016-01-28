532d702bdc47a4c621fbe1249d765770c1db43a6
Takeru Komiya <tkomiya@hotmail.com>
2016-01-28 15:51:26 -0800

    Removes the methods moved to a different module

--- app/controllers/vocablists.server.controller.js
+++ app/controllers/vocablists.server.controller.js
@@ -10,49 +10,6 @@ var _ = require('lodash');
 var Vocab = mongoose.model('Vocab');
 var updateVocabHelper = require('../helpers/updateVocab');
 
-function findAndUpdateVocab(vocab) {
-  if (vocab.deleted) {
-    return new Promise(function(resolve, reject) {
-      Vocab.findByIdAndRemove(
-        vocab._id,
-        function(err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
-  } else if (vocab._id) {
-    return new Promise(function(resolve, reject) {
-      Vocab.findByIdAndUpdate(
-        vocab._id,
-        vocab,
-        function(err, doc) {
-          if (err) {
-            reject(err);
-          } else if (!doc) {
-            reject(new Error('Failed to update vocab ' + vocab.word));
-          } else {
-            resolve(doc);
-          }
-        }
-      );
-    });
-  } else {
-    return Vocab.create(vocab);
-  }
-}
-
-function findAndUpdateVocabs(vocabs) {
-  var promises = [];
-  for (var i = 0; i < vocabs.length; i++) {
-    promises.push(findAndUpdateVocab(vocabs[i]));
-  }
-  return Promise.all(promises);
-}
-
 function saveVocablistPromise(vocablist) {
   return new Promise(function(resolve, error) {
     vocablist.save(function(err) {
