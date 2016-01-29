1fcdadc2de80c79ff2c069250596392fd8678374
Takeru Komiya <tkomiya@hotmail.com>
2016-01-28 19:36:53 -0800

    Adds the category constants

--- public/modules/vocablists/controllers/createVocablist.client.controller.js
+++ public/modules/vocablists/controllers/createVocablist.client.controller.js
@@ -2,11 +2,18 @@
 
 angular.module('vocablists').
   controller('CreateVocablistController',
-    ['$scope', '$stateParams', '$location', 'Authentication', 'Vocablists',
-    function($scope, $stateParams, $location, Authentication, Vocablists) {
+    ['$scope', '$stateParams', '$location', 'Authentication', 'Vocablists', 'Category',
+    function($scope, $stateParams, $location, Authentication, Vocablists, Category) {
 
       $scope.vocablist = new Vocablists();
       $scope.vocablist.vocab = [];
+      $scope.categories = [];
+
+      for (var property in Category) {
+        if (Category.hasOwnProperty(property)) {
+          $scope.categories.push(Category[property]);
+        }
+      }
 
       $scope.addVocab = function() {
         $scope.vocablist.vocab.push({});
