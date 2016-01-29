b98f993ef51a837bf0fdb024856f800bc53f054c
Takeru Komiya <tkomiya@hotmail.com>
2016-01-28 22:33:13 -0800

    Adds a resolve to the home route

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -1,15 +1,26 @@
 'use strict';
 
 // Setting up route
-angular.module('core').config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+angular.module('core').config(['$stateProvider', '$urlRouterProvider', 'Constants',
+  function($stateProvider, $urlRouterProvider, Constants) {
     // Redirect to home view when route not found
     $urlRouterProvider.otherwise('/');
 
     // Home state routing
     $stateProvider.state('home', {
       url: '/',
-      templateUrl: 'modules/core/views/home.client.view.html'
+      templateUrl: 'modules/core/views/home.client.view.html',
+      resolve: {
+        checkAuthentication: function(Authentication, $q) {
+          return $q(function(resolve, reject) {
+            if (!Authentication.currentUser()) {
+              resolve();
+            } else {
+              reject(Constants.AUTHORIZED_REROUTE);
+            }
+          });
+        }
+      }
     });
   }
 ]);
