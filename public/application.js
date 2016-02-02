b95708fb7061427b83a7620ffae904c322c4c318
Takeru Komiya <tkomiya@hotmail.com>
2016-02-01 22:09:26 -0800

    Adds a directive that affixes elements

--- public/application.js
+++ public/application.js
@@ -56,4 +56,20 @@ angular.module(ApplicationConfiguration.applicationModuleName).
     $rootScope.$on('$stateChangeSuccess', function() {
       $rootScope.authenticationError = null;
     });
-  }]);
+  }]).directive('setClassWhenAtTop', function($window) {
+      var $win = angular.element($window); // wrap window object as jQuery object
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var topClass = attrs.setClassWhenAtTop; // get CSS class from directive's attribute value
+          var offsetTop = element.offset().top; // get element's offset top relative to document
+          $win.on('scroll', function(e) {
+            if ($win.scrollTop() >= offsetTop) {
+              element.addClass(topClass);
+            } else {
+              element.removeClass(topClass);
+            }
+          });
+        }
+      };
+    });
