167bdc9b72ecce4c492c0c4a817f3f219d492a6b
Takeru Komiya <tkomiya@hotmail.com>
2016-02-02 17:54:35 -0800

    Implements an empty strategy

--- config/strategies/remember-me.js
+++ config/strategies/remember-me.js
@@ -1,1 +1,18 @@
-var RememberMe = require('passport-remember-me');
+'use strict';
+
+var RememberMe = require('passport-remember-me').Strategy;
+var passport = require('passport');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Token = mongoose.model('Token');
+
+module.exports = function() {
+  passport.use(new RememberMe(
+    function(token, done) {
+
+    },
+    function(token, done) {
+
+    }
+  ));
+};
