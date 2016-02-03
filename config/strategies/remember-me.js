705bb9a9e61f43fb50d2e27de814d37546c6d0ce
Takeru Komiya <tkomiya@hotmail.com>
2016-02-02 18:08:24 -0800

    Implements the remember-me strategy

--- config/strategies/remember-me.js
+++ config/strategies/remember-me.js
@@ -5,14 +5,34 @@ var passport = require('passport');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Token = mongoose.model('Token');
+var tokenGenerator = require('rand-token');
+
+function getUserFromToken(token, done) {
+  Token.find({token: token}, {user: 1}, null).populate('user', function(err, token) {
+    if (err) {
+      done(err);
+    } else if (!token) {
+      done(null, false);
+    } else {
+      done(null, token.user);
+    }
+  });
+}
+
+function issueTokenToUser(user, done) {
+  var token = tokenGenerator.generate(64);
+  Token.create({token: token, user: user}, function(err, token) {
+    if (err) {
+      done(err);
+    } else {
+      done(null, token);
+    }
+  });
+}
 
 module.exports = function() {
   passport.use(new RememberMe(
-    function(token, done) {
-
-    },
-    function(token, done) {
-
-    }
+    getUserFromToken,
+    issueTokenToUser
   ));
 };
