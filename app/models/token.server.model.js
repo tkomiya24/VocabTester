98e0093d5f0bef774ec7f9714d870be18348e2cd
Takeru Komiya <tkomiya@hotmail.com>
2016-02-02 17:50:22 -0800

    Implements the token model

--- app/models/token.server.model.js
+++ app/models/token.server.model.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var tokenSchema = new Schema({
+  token: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true
+  }
+});
+
+mongoose.model('Token', tokenSchema);
