a7f2ea3e0b18d1acd84b160370240ce8b2c3ac1c
Takeru Komiya <tkomiya@hotmail.com>
2016-02-12 16:48:46 -0800

    Removes the buildCSS task

--- gruntfile.js
+++ gruntfile.js
@@ -194,7 +194,6 @@ module.exports = function(grunt) {
 
   // Build task(s).
   grunt.registerTask('build', ['lint', 'sass', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
-  grunt.registerTask('buildCSS', ['loadConfig', 'sass', 'cssmin']);
 
   // Test task.
   grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
