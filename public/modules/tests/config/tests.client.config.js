8cf3f3125a717b2fc4c6250567a8bb8732201ca8
Takeru Komiya <tkomiya@hotmail.com>
2016-04-17 15:30:19 -0700

    Adds a toolbar option for least tested

--- public/modules/tests/config/tests.client.config.js
+++ public/modules/tests/config/tests.client.config.js
@@ -2,9 +2,10 @@
 
 // Configuring the Articles module
 angular.module('tests').run(['Menus',
-	function(Menus) {
+  function(Menus) {
     // Set top bar menu items
     Menus.addMenuItem('topbar', 'Tests', 'tests', 'dropdown', '/tests(/create)?');
     Menus.addSubMenuItem('topbar', 'tests', 'Most mistaken vocab', 'vocablists/test/most-mistaken');
+    Menus.addSubMenuItem('topbar', 'tests', 'Least tested vocab', 'vocablists/test/leastTested');
   }
 ]);
